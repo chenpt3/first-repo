a0cc0bcf0d843547c2ff1ecfd20c1c41b61e15ca
Chen <chenpt3@gmail.com>
2023-11-26 16:48:37 +0200

    .

--- rock-paper-scisors/rps-scripts.js
+++ rock-paper-scisors/rps-scripts.js
@@ -1,4 +1,9 @@
-
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+let outputText1 = document.getElementById("output-text-1");
+let outputText2 = document.getElementById("output-text-2");
+let outputText3 = document.getElementById("output-text-3");
 
 function getComputerChoice() {
     let computerChoice;
@@ -15,27 +20,40 @@ function getComputerChoice() {
         computerChoice = "Scissors"
         return computerChoice
     } else {}
-}
+};
 
-function game(playerSelection, computerChoice) {
+function checkWin(playerSelection, computerChoice) {
     let roundScore;
     if (playerSelection === computerChoice) {
         roundScore = "It's a Tie!"
-    } else if (playerSelection === "rock" && computerChoice === "paper") {
+    } else if (playerSelection === "Rock" && computerChoice === "Paper") {
         roundScore = "Computer Wins!"
-    } else if (playerSelection === "paper" && computerChoice === "scissors") {
+    } else if (playerSelection === "Paper" && computerChoice === "Scissors") {
         roundScore = "Computer Wins!"
-    } else if (playerSelection === "scissors" && computerChoice === "rock") {
+    } else if (playerSelection === "Scissors" && computerChoice === "Rock") {
         roundScore = "Computer Wins!"
-    } else if (playerSelection === "scissors" && computerChoice === "paper") {
-        roundScore = "Player Wins!"
-    }  else if (playerSelection === "paper" && computerChoice === "rock") {
-        roundScore = "Player Wins!"
-    }  else if (playerSelection === "rock" && computerChoice === "scissors") {
-        roundScore = "Player Wins!"
+    } else if (playerSelection === "Scissors" && computerChoice === "Paper") {
+        roundScore = "You win!"
+    }  else if (playerSelection === "Paper" && computerChoice === "Rock") {
+        roundScore = "You win!"
+    }  else if (playerSelection === "Rock" && computerChoice === "Scissors") {
+        roundScore = "You win!"
     } else {}
-    return roundScore
+    return(roundScore)
 }
 
+function game(userInput) {
+    outputText1.innerText = `You chose: ${userInput.innerText}!`;
+    let computerChoice = getComputerChoice();
+    outputText2.innerText = `Computer chose: ${computerChoice}!`;
+    outputText3.innerText = checkWin(userInput.innerText, computerChoice);
+}
 
-console.log(game())
+[...document.getElementsByClassName("input-btn")].forEach(x => {
+    x.addEventListener('click', function(){
+        game(this)
+    })
+})
+
+
+
