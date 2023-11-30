666901c6b8782cea60590856644f318dcf124ea8
Chen <chenpt3@gmail.com>
2023-11-30 07:38:36 +0200

    sketch!

--- sketch/sketch-script.js
+++ sketch/sketch-script.js
@@ -6,29 +6,20 @@ const penButton = document.querySelector(".pen");
 const eraseButton = document.querySelector(".eraser");
 const colorButton = document.querySelector(".random-color");
 const darknerButton = document.querySelector(".darkner");
-const darknerValue = document.getElementById("darkner");
 const brightnerButton = document.querySelector(".brightner");
-const brightnerValue = document.getElementById("brightner");
+const backColor = document.getElementById("background-color");
+const getColorButton = document.querySelector(".get-color");
+const fillButton = document.querySelector(".fill");
 
 let isBorder = true;
-let isPen = true;
+let isPen = false;
 let isPenActive = false;
 let isEraser = false;
 let isColor = false;
 let isDarkner = false;
 let isBrightner = false;
-
-function darkenNum(i, num) {
-    if (i>=0 && i<256) {
-        return i-num;
-    };
-};
-
-function brightenNum(i, num) {
-    if (i>=0 && i<256) {
-        return i+num;
-    };
-};
+let isFill = false;
+let isGetColor = false;
 
 function getRandomColor() {
     min = Math.ceil(0);
@@ -38,6 +29,20 @@ function getRandomColor() {
 
 function initGrid() {
     let draw = gridValue.addEventListener("mouseup", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
+        isPen = false;
+        isFill = false;
+        isGetColor = false;
+        isEraser = false;
+        isColor = false;
+        isDarkner = false;
+        isBrightner = false;
         const gridSize = parseInt(gridValue.value);
         gameContainer.innerHTML = "";
         for (i=0; i<(gridSize*gridSize); i++) {
@@ -75,17 +80,24 @@ function toggleGrid() {
 
 function resetGrid() {
     const reset = resetButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
         isBorder = true;
         isPen = true;
         isEraser = false;
         isColor = false;
         isDarkner = false;
         isBrightner = false;
-        gridValue.value = 32;
-        darknerValue.value = 10;
-        brightnerValue.value = 10;
+        penColor.value = "black";
+        backColor.value = "#f3f3f3";
+        gridValue.value = 16;
         gameContainer.innerHTML = "";
-        for (i=0; i<(32*32); i++) {
+        for (i=0; i<(16*16); i++) {
             let cell = document.createElement("div");
             cell.classList.add("cell");
             cell.classList.add("border");
@@ -93,19 +105,16 @@ function resetGrid() {
         };
         const cells = Array.from(document.querySelectorAll(".cell"));
         cells.forEach(cell => {
-            cell.style.width = `${100/32}%`;
-            cell.style.height = `${100/32}%`;
+            cell.style.width = `${100/16}%`;
+            cell.style.height = `${100/16}%`;
         });
     });
-    return reset;
 };
 
 function start() {
-    gridValue.value = 32;
-    darknerValue.value = 10;
-    brightnerValue.value = 10;
+    gridValue.value = 16;
     gameContainer.innerHTML = "";
-    for (i=0; i<(32*32); i++) {
+    for (i=0; i<(16*16); i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
         cell.classList.add("border");
@@ -113,14 +122,23 @@ function start() {
     };
     const cells = Array.from(document.querySelectorAll(".cell"));
     cells.forEach(cell => {
-        cell.style.width = `${100/32}%`;
-        cell.style.height = `${100/32}%`;
+        cell.style.width = `${100/16}%`;
+        cell.style.height = `${100/16}%`;
     });
 };
 
 function pen() {
     const pen = penButton.addEventListener("click", function() {
+        penButton.classList.add("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
         isPen = true;
+        isFill = false;
+        isGetColor = false;
         isEraser = false;
         isColor = false;
         isDarkner = false;
@@ -129,6 +147,9 @@ function pen() {
         cells.forEach(cell => {
         cell.addEventListener("mousedown", function(){
             isPenActive = true;
+            if (isPen && isPenActive) {
+                cell.style.backgroundColor = penColor.value;
+            };
         });
         cell.addEventListener("mouseup", function() {
             isPenActive = false;
@@ -137,9 +158,6 @@ function pen() {
             if (isPen && isPenActive) {
                 cell.style.backgroundColor = penColor.value;
             };
-        cell.addEventListener("click", function() {
-            cell.style.backgroundColor = penColor.value;
-        });
         });
       });
     });
@@ -147,7 +165,16 @@ function pen() {
 
 function eraser() {
     const eraser = eraseButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.add("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
         isPen = false;
+        isFill = false;
+        isGetColor = false;
         isEraser = true;
         isColor = false;
         isDarkner = false;
@@ -156,17 +183,17 @@ function eraser() {
         cells.forEach(cell => {
         cell.addEventListener("mousedown", function(){
             isPenActive = true;
+            if (isEraser && isPenActive) {
+                cell.style.backgroundColor = backColor.value;
+            };
         });
         cell.addEventListener("mouseup", function() {
             isPenActive = false;
         });
         cell.addEventListener("mouseover", function() {
             if (isEraser && isPenActive) {
-                cell.style.backgroundColor = "rgb(200, 200, 200)";
+                cell.style.backgroundColor = backColor.value;
             };
-        cell.addEventListener("click", function() {
-            cell.style.backgroundColor = "rgb(200, 200, 200)";
-        });
         });
       });
     });
@@ -174,7 +201,16 @@ function eraser() {
 
 function color() {
     const color = colorButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.add("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
         isPen = false;
+        isFill = false;
+        isGetColor = false;
         isEraser = false;
         isColor = true;
         isDarkner = false;
@@ -183,6 +219,12 @@ function color() {
         cells.forEach(cell => {
         cell.addEventListener("mousedown", function(){
             isPenActive = true;
+            if (isColor && isPenActive) {
+                let r = getRandomColor();
+                let g = getRandomColor();
+                let b = getRandomColor();
+                cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            };
         });
         cell.addEventListener("mouseup", function() {
             isPenActive = false;
@@ -194,12 +236,6 @@ function color() {
                 let b = getRandomColor();
                 cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
             };
-        cell.addEventListener("click", function() {
-            let r = getRandomColor();
-            let g = getRandomColor();
-            let b = getRandomColor();
-            cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        });
         });
       });
     });
@@ -207,7 +243,16 @@ function color() {
 
 function darkner() {
     const darkner = darknerButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.add("clicked");
+        brightnerButton.classList.remove("clicked");
         isPen = false;
+        isFill = false;
+        isGetColor = false;
         isEraser = false;
         isColor = false;
         isDarkner = true;
@@ -216,6 +261,15 @@ function darkner() {
         cells.forEach(cell => {
         cell.addEventListener("mousedown", function(){
             isPenActive = true;
+            if (isDarkner && isPenActive) {
+                let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
+                cellColor = cellColor.slice(4,-1);
+                cellColor = cellColor.split(", ");
+                let r = parseInt(cellColor[0]);
+                let g = parseInt(cellColor[1]);
+                let b = parseInt(cellColor[2]);
+                cell.style.backgroundColor = `rgb(${r-10}, ${g-10}, ${b-10})`;
+            };
         });
         cell.addEventListener("mouseup", function() {
             isPenActive = false;
@@ -223,32 +277,30 @@ function darkner() {
         cell.addEventListener("mouseover", function() {
             if (isDarkner && isPenActive) {
                 let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
-                let num = parseInt(darknerValue.value);
                 cellColor = cellColor.slice(4,-1);
                 cellColor = cellColor.split(", ");
                 let r = parseInt(cellColor[0]);
                 let g = parseInt(cellColor[1]);
                 let b = parseInt(cellColor[2]);
-                cell.style.backgroundColor = `rgb(${darkenNum(r, num)}, ${darkenNum(g, num)}, ${darkenNum(b, num)})`;
+                cell.style.backgroundColor = `rgb(${r-10}, ${g-10}, ${b-10})`;
             };
-        cell.addEventListener("click", function() {
-            let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
-                let num = parseInt(darknerValue.value);
-                cellColor = cellColor.slice(4,-1);
-                cellColor = cellColor.split(", ");
-                let r = parseInt(cellColor[0]);
-                let g = parseInt(cellColor[1]);
-                let b = parseInt(cellColor[2]);
-                cell.style.backgroundColor = `rgb(${darkenNum(r, num)}, ${darkenNum(g, num)}, ${darkenNum(b, num)})`;
         });
         });
-      });
     });
 };
-
+       
 function brightner() {
     const brightner = brightnerButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.add("clicked");
         isPen = false;
+        isFill = false;
+        isGetColor = false;
         isEraser = false;
         isColor = false;
         isDarkner = false;
@@ -257,6 +309,15 @@ function brightner() {
         cells.forEach(cell => {
         cell.addEventListener("mousedown", function(){
             isPenActive = true;
+            if (isBrightner && isPenActive) {
+                let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
+                cellColor = cellColor.slice(4,-1);
+                cellColor = cellColor.split(", ");
+                let r = parseInt(cellColor[0]);
+                let g = parseInt(cellColor[1]);
+                let b = parseInt(cellColor[2]);
+                cell.style.backgroundColor = `rgb(${r+10}, ${g+10}, ${b+10})`;
+            };
         });
         cell.addEventListener("mouseup", function() {
             isPenActive = false;
@@ -264,28 +325,122 @@ function brightner() {
         cell.addEventListener("mouseover", function() {
             if (isBrightner && isPenActive) {
                 let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
-                let num = parseInt(brightnerValue.value);
                 cellColor = cellColor.slice(4,-1);
                 cellColor = cellColor.split(", ");
                 let r = parseInt(cellColor[0]);
                 let g = parseInt(cellColor[1]);
                 let b = parseInt(cellColor[2]);
-                cell.style.backgroundColor = `rgb(${brightenNum(r, num)}, ${brightenNum(g, num)}, ${brightenNum(b, num)})`;
+                cell.style.backgroundColor = `rgb(${r+10}, ${g+10}, ${b+10})`;
             };
-        cell.addEventListener("click", function() {
-            let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
-                let num = parseInt(brightnerValue.value);
-                cellColor = cellColor.slice(4,-1);
-                cellColor = cellColor.split(", ");
-                let r = parseInt(cellColor[0]);
-                let g = parseInt(cellColor[1]);
-                let b = parseInt(cellColor[2]);
-                cell.style.backgroundColor = `rgb(${brightenNum(r, num)}, ${brightenNum(g, num)}, ${brightenNum(b, num)})`;
-        });
         });
       });
     });
-}
+};
+
+function backgroundChange() {
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = backColor.value;
+    });
+};
+
+function getColor() {
+    const get = getColorButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.remove("clicked");
+        getColorButton.classList.add("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
+        isPen = false;
+        isFill = false;
+        isGetColor = true;
+        isEraser = false;
+        isColor = false;
+        isDarkner = false;
+        isBrightner = false;
+        const cells = Array.from(document.querySelectorAll(".cell"));
+        cells.forEach(cell => {
+            cell.addEventListener("click", function() {
+                let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
+                penColor.value = colorStringToHex(cellColor);
+            });
+        });
+    });
+};
+
+function rgbToHex(num) {
+    let hex = num.toString(16);
+    return hex;
+};
+
+function fill() {
+    const fill = fillButton.addEventListener("click", function() {
+        penButton.classList.remove("clicked");
+        eraseButton.classList.remove("clicked");
+        fillButton.classList.add("clicked");
+        getColorButton.classList.remove("clicked");
+        colorButton.classList.remove("clicked");
+        darknerButton.classList.remove("clicked");
+        brightnerButton.classList.remove("clicked");
+        isPen = false;
+        isFill = true;
+        isGetColor = false;
+        isEraser = false;
+        isColor = false;
+        isDarkner = false;
+        isBrightner = false;
+        const cells = Array.from(document.querySelectorAll(".cell"));
+        cells.forEach(cell => {
+            cell.addEventListener("click", function() {
+                let neighbors = Array.from(findNeighbors(cells, cell));
+                let mainCellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
+                let oldColor = colorStringToHex(mainCellColor);
+                let num = parseInt(gridValue.value);
+                for (i=0; i<num; i++) {
+                    neighbors.forEach(cell => {
+                        let cellColor = window.getComputedStyle(cell).getPropertyValue("background-color");
+                        cellColor = colorStringToHex(cellColor);
+                        if (cellColor === oldColor) {
+                            cell.style.backgroundColor = penColor.value;
+                            let secondNeighbors = Array.from(findNeighbors(cells, cell));
+                            secondNeighbors.forEach(element => {
+                                let elementColor = window.getComputedStyle(element).getPropertyValue("background-color");
+                                elementColor = colorStringToHex(elementColor);
+                                if (elementColor === oldColor && !neighbors.includes(element)) {
+                                    neighbors.push(element);
+                                };  
+                            });
+                        };
+                    });
+                }
+            });
+        });
+        
+    });
+};
+
+function colorStringToHex(cellColor) {
+    cellColor = cellColor.slice(4,-1);
+    cellColor = cellColor.split(", ");
+    let r = parseInt(cellColor[0]);
+    let g = parseInt(cellColor[1]);
+    let b = parseInt(cellColor[2]);
+    let cellHex = `#${rgbToHex(r)}${rgbToHex(g)}${rgbToHex(b)}`
+    return cellHex;
+};
+
+function findNeighbors(cells, cell) {
+    const width = parseInt(gridValue.value);
+    let i = cells.indexOf(cell);
+    let topRow = cells.slice(i-width-1, i-width+2);
+    let bottomRow = cells.slice(i+width-1, i+width+2);
+    let middleRow = cells.slice(i-1,i+2);
+    let neigh = topRow.concat(bottomRow, middleRow);
+    return neigh
+};
+
 
 start();
 initGrid();
@@ -296,7 +451,6 @@ eraser();
 color();
 darkner();
 brightner();
-
-
-
+getColor();
+fill();
 
