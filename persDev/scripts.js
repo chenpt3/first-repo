27f0138c2a65236f6ff04b517c60afd4dfc75416
Chen <chenpt3@gmail.com>
2024-04-03 05:13:15 +0300

    made sense of the repositories

--- persDev/scripts.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById("sign-form");
-const fname = document.getElementById("first-name");
-
-fname.addEventListener("input", (event) => {
-    if (fname.validity.valid)
-})
